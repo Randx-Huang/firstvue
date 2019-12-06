2cd98303e4284b2394530ae1cca0b99b427d4cd4
RandxHuang <wind04181986@gmail.com>
2019-12-07 00:04:00 +0800

    edit About.vue for test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    {{ textWord }} //add this
   </div>
 </template>
+
+// add whole script
+<script>
+export default {
+  data() {
+    return {
+      textWord: "my first vue app"
+    };
+  }
+};
+</script>
